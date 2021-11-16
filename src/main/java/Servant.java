c742bdbf80cb43cf0bbf5ca34b5a9af74e286203
EmRyom <61932752+EmRyom@users.noreply.github.com>
2021-11-16 10:47:36 +0100

    RBAC no clutter

--- src/main/java/Servant.java
+++ src/main/java/Servant.java
@@ -17,23 +17,9 @@ import org.mindrot.jbcrypt.BCrypt;
 
 
 public class Servant extends UnicastRemoteObject implements Print {
-
-    ArrayList<Printer> printers = new ArrayList<>();
-    HashMap<String,String> parameters= new HashMap<String,String>();
     HashMap<String,String[]> users = new HashMap<>();
     HashMap<String,String[]> permissions = new HashMap<String,String[]>();
 
-
-
-    private int FindPrinter (String printer) {
-        for (int i = printers.size()-1; i > -1; i--) {
-            if (printers.get(i).name.equals(printer)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    
     private Boolean hasPermission (String user, String action) {
         String[] roles = users.get(user);
         boolean rec = false;
@@ -90,15 +76,7 @@ public class Servant extends UnicastRemoteObject implements Print {
     @Override
     public String print(String user, String filename, String printer) throws RemoteException {
         if (hasPermission(user,"print")) {
-            int i = FindPrinter(printer);
-            if (i==-1) {
-                Printer p = new Printer(printer);
-                p.addToQueue(filename);
-                this.printers.add(p);
-            } else {
-                printers.get(i).addToQueue(filename);
-            }
-            System.out.println("Print: "+filename+" on "+printer);
+            System.out.println("Print");
             return "Print successful";
         } else {
             return "You do not have permission to print";
@@ -108,17 +86,8 @@ public class Servant extends UnicastRemoteObject implements Print {
     @Override
     public String queue(String user, String printer) throws RemoteException {
         if (hasPermission(user,"queue")) {
-            System.out.println("Queue: "+printer);
-            int u = FindPrinter(printer);
-            if (u==-1) {
-                return "Printer "+printer+" doesn't exist";
-            }
-            ArrayList queue = printers.get(u).Queue;
-            String s ="Job|Filename\n";
-            for (int i = queue.size(); i>0; i--) {
-                s=s+((queue.size()-i+1)+"|"+queue.get(queue.size()-i)+"\n");
-            }
-            return s;
+            System.out.println("Queue");
+            return "Queue successful";
         } else {
             return "You do not have permission to queue";
         }
@@ -129,16 +98,7 @@ public class Servant extends UnicastRemoteObject implements Print {
     public String topQueue(String user, String printer, int job) throws RemoteException {
         if (hasPermission(user,"topQueue")) {
             System.out.println("Top queue for job "+job+" on "+printer+" was demanded");
-            int i = FindPrinter(printer);
-            if (i==-1) {
-                return "Cannot more job "+job+" as "+printer+" doesn't exist";
-            } else {
-                if (printers.get(i).moveToTop(job)) {
-                    return "topQueue for job "+job+" on printer "+printer+" successful";
-                } else {
-                    return "Cannot more job "+job+" on "+printer+", index too high";
-                }
-            }
+            return "Top queue successful";
         } else {
             return "You do not have permission to topQueue";
         }
@@ -148,13 +108,7 @@ public class Servant extends UnicastRemoteObject implements Print {
     @Override
     public String start(String user) throws RemoteException {
         if (hasPermission(user,"start")) {
-            System.out.println("Start server (connecting to printer1, printer2 and printer3)");
-            Printer p1 = new Printer("printer1");
-            Printer p2 = new Printer("printer2");
-            Printer p3 = new Printer("printer3");
-            printers.add(p1);
-            printers.add(p2);
-            printers.add(p3);
+            System.out.println("Start");
             return "Start successful";
         } else {
             return "You do not have permission to start";
@@ -165,8 +119,7 @@ public class Servant extends UnicastRemoteObject implements Print {
     @Override
     public String stop(String user) throws RemoteException {
         if (hasPermission(user,"stop")) {
-            printers = new ArrayList<>();
-            System.out.println("Stop server (Disconnecting from all printers)");
+            System.out.println("Stop");
             return "Stop successful";
         } else {
             return "You do not have permission to stop";
@@ -176,13 +129,6 @@ public class Servant extends UnicastRemoteObject implements Print {
     @Override
     public String restart(String user) throws RemoteException {
         if (hasPermission(user,"restart")) {
-            printers = new ArrayList<>();
-            Printer p1 = new Printer("printer1");
-            Printer p2 = new Printer("printer2");
-            Printer p3 = new Printer("printer3");
-            printers.add(p1);
-            printers.add(p2);
-            printers.add(p3);
             System.out.println("Restart");
             return "Restart successful";
         } else {
@@ -193,13 +139,8 @@ public class Servant extends UnicastRemoteObject implements Print {
     @Override
     public String status(String user, String printer) throws RemoteException {
         if (hasPermission(user,"status")) {
-            System.out.println("Status: "+printer);
-            int i = FindPrinter(printer);
-            if (i==-1) {
-                return "Printer "+printer+" doesn't exist";
-            } else {
-                return printers.get(i).status();
-            }
+            System.out.println("Status");
+            return "Status successful";
         } else {
             return "You do not have permission to status";
         }
@@ -208,13 +149,8 @@ public class Servant extends UnicastRemoteObject implements Print {
     @Override
     public String readConfig(String user, String parameter) throws RemoteException {
         if (hasPermission(user,"readConfig")) {
-            System.out.println("Read config: "+parameter);
-            String out = parameters.get(parameter);
-            if (out!=null) {
-                return parameter+" is set to "+out;
-            } else {
-                return parameter+" doesn't exist";
-            }
+            System.out.println("Read config");
+            return "Read config successful";
         } else {
             return "You do not have permission to readConfig";
         }
@@ -224,9 +160,8 @@ public class Servant extends UnicastRemoteObject implements Print {
     @Override
     public String setConfig(String user, String parameter, String value) throws RemoteException {
         if (hasPermission(user,"setConfig")) {
-            System.out.println("Set config: "+parameter+" to "+value);
-            parameters.put(parameter,value);
-            return "Parameter "+parameter+" set to "+value;
+            System.out.println("Set config");
+            return "Set config successful";
         } else {
             return "You do not have permission to setConfig";
         }
